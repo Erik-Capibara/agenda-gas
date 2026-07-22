399ff950a2ee1b0d7806966ab289757549db9a8a
Erik <pauerikl24@gmail.com>
2026-07-22 17:31:24 -0500

    video9 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -7,3 +7,10 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('1jRl0YxrBSrQuv09W53iDkRJWTIGNkJJZr8lm77gE77k').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
